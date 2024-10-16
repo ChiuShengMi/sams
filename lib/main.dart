b2021e63d3597646ea24527fea7b558cf69b0161
East <adyureew956@gmail.com>
2024-10-16 11:12:26 +0900

    structure settle

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
+import './pages/mainPages/homePage.dart'; // Homeページのパスを設定
+import './pages/loginPages/login.dart'; // Loginページのパスを設定
 
 void main() async {
-  
   WidgetsFlutterBinding.ensureInitialized();
   
-  
+  // Firebase初期化
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -19,14 +21,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Firebase App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Firebase Initialized'),
-        ),
-        body: Center(
-          child: Text('Firebase Initialization Successful!'),
-        ),
-      ),
+      home: AuthCheck(),
+    );
+  }
+}
+
+class AuthCheck extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(), // ユーザーのログイン状態を監視
+      builder: (context, snapshot) {
+        // ログイン状態を確認
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasData) {
+          // ログインしている場合、HomePageに遷移
+          return HomePage();
+        } else {
+          // ログインしていない場合、LoginPageに遷移
+          return LoginPage();
+        }
+      },
     );
   }
 }
---
+++ lib/pages/loginPages/login.dart
@@ -0,0 +1,16 @@
+// login.dart
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Center(
+        child: Text('Please log in to continue'),
+      ),
+    );
+  }
+}
