53fb1fef67793bfe2baecea1bef9741b76c30511
dong <ttiyong0320@naver.com>
2024-10-26 18:47:37 +0900

    actionbar 実装

---
+++ lib/widget/actionbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Actionbar extends StatelessWidget {
+  Actionbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(30),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 0.1),
+          borderRadius: BorderRadius.circular(10)),
+      height: 100,
+      child: Container(margin: EdgeInsets.all(30)),
+    );
+  }
+}
