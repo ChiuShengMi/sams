e606aae016f5701b9bfed9af5a2fdce8e2da07b7
AdiyaDede <gantulga.adiyadelger@gmail.com>
2024-10-30 14:25:39 +0900

    show_lesson_table

--- lib/pages/mainPages/subjectlist/subjecttable.dart
+++ lib/pages/mainPages/subjectlist/subjecttable.dart
@@ -1,3 +1,4 @@
+import 'package:sams/pages/mainPages/homepage_admin.dart';
 import 'package:sams/widget/appbar.dart';
 import 'package:sams/widget/bottombar.dart';
 import 'package:sams/widget/lessontable.dart';
@@ -9,26 +10,45 @@ class SubjectTable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(), // Apply custom AppBar
+      appBar: CustomAppBar(),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
-            "授業リスト",
-            style: TextStyle(
-              fontSize: 24, // Customize font size
-              fontWeight: FontWeight.bold, // Make the font bold
-              color: Colors.black, // Adjust the color as needed
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "授業リスト",
+                style: TextStyle(
+                  fontSize: 24, // Customize font size
+                  fontWeight: FontWeight.bold, // Make the font bold
+                  color: Colors.black, // Adjust the color as needed
+                ),
+              ),
+              ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePageAdmin()),
+                  );
+                },
+                icon: Icon(Icons.turn_left),
+                label: Text("Back Home"),
+              ),
+            ],
           ),
+
+          SizedBox(height: 20), // Add spacing below the Row
+
           Divider(
             color: Colors.grey,
             thickness: 1.5,
             height: 15.0,
           ),
 
-          // Add spacing below the Divider
-          SizedBox(height: 20), // Add 20 pixels of vertical space
+          SizedBox(height: 20), // Add more space after Divider if needed
+
+          // Add 20 pixels of vertical space
 
           // Rest of your content
           Expanded(
