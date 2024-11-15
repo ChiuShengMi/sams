08c0700f1ed290c9c58263a5d38bfa9ba886056f
dong <ttiyong0320@naver.com>
2024-11-15 14:27:05 +0900

    ユーザ情報

--- lib/pages/user/detail.dart
+++ lib/pages/user/detail.dart
@@ -3,85 +3,163 @@ import 'package:sams/widget/actionbar.dart';
 import 'package:sams/widget/appbar.dart';
 import 'package:sams/widget/button/custom_button.dart';
 import 'package:sams/widget/custom_input_container.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-
+import 'package:sams/pages/user/edit.dart';
+import 'package:sams/pages/user/list.dart';
+import 'package:sams/widget/modal/delete_modal.dart'; // DeleteModal import
 
 class UserDetail extends StatelessWidget {
-  final TextEditingController loginEmailInputController =
-      TextEditingController(); // Login E-mail Id
-  final TextEditingController dataIdInputController =
-      TextEditingController(); // Data Id
-  final TextEditingController userNameInputController =
-      TextEditingController(); // User Name
-  final TextEditingController phoneNumberController =
-      TextEditingController(); // Phone Number
+  final String documentPath;
+
+  UserDetail({required this.documentPath});
+
+  Future<Map<String, dynamic>?> _fetchUserData() async {
+    try {
+      DocumentSnapshot snapshot =
+          await FirebaseFirestore.instance.doc(documentPath).get();
+      return snapshot.data() as Map<String, dynamic>?;
+    } catch (e) {
+      print("Error fetching user data: $e");
+      return null;
+    }
+  }
+
+  Future<void> _deleteUser(BuildContext context) async {
+    try {
+      await FirebaseFirestore.instance.doc(documentPath).delete();
+
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("User deleted successfully!"),
+        backgroundColor: Colors.green,
+      ));
+
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => UserList()),
+        (route) => false,
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Failed to delete user: $e"),
+        backgroundColor: Colors.red,
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Actionbar(children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [SmallButton(text: '修正', onPressed: () {})],
-                )
-              ]),
-              CustomInputContainer(
-                title: 'User Detail',
-                inputWidgets: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(flex: 5, child: Text('Login E-mail')),
-                      SizedBox(width: 16),
-                      Expanded(flex: 1, child: Text('property')),
+      body: FutureBuilder<Map<String, dynamic>?>(
+        future: _fetchUserData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (!snapshot.hasData || snapshot.data == null) {
+            return Center(child: Text('No data found'));
+          }
+
+          var userData = snapshot.data!;
+
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Actionbar(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        CustomButton(
+                          text: '修正',
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => UserEdit(
+                                  documentPath: documentPath,
+                                  loginEmail: userData['MAIL'] ?? 'N/A',
+                                  dataId: userData['ID']?.toString() ?? 'N/A',
+                                  userName: userData['NAME'] ?? 'N/A',
+                                  phoneNumber: userData['TEL'] ?? 'N/A',
+                                  className: userData['CLASS'] ?? 'N/A',
+                                  role: userData['JOB'] ?? 'N/A',
+                                  course: userData['COURSE'] ?? 'N/A',
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                        SizedBox(width: 30),
+                        CustomButton(
+                          text: '削除',
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => DeleteModal(
+                                onConfirm: () async {
+                                  Navigator.of(context).pop(); // 모달 닫기
+                                  await _deleteUser(context);
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ]),
+                  CustomInputContainer(
+                    title: 'User Detail',
+                    inputWidgets: [
+                      _buildDetailRow(
+                          'Login E-mail', userData['MAIL'] ?? 'N/A'),
+                      _buildDetailRow(
+                          'Data ID', userData['ID']?.toString() ?? 'N/A'),
+                      _buildDetailRow('User Name', userData['NAME'] ?? 'N/A'),
+                      _buildDetailRow('Phone Number', userData['TEL'] ?? 'N/A'),
+                      _buildDetailRow('Course', userData['COURSE'] ?? 'N/A'),
+                      _buildDetailRow('Class', userData['CLASS'] ?? 'N/A'),
+                      _buildDetailRow('Role', userData['JOB'] ?? 'N/A'),
                     ],
                   ),
-                  SizedBox(height: 16),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      Expanded(flex: 2, child: Text('data Id')),
-                      SizedBox(width: 16),
-                      Expanded(flex: 2, child: Text('User Name')),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(flex: 2, child: Text('Phone Number')),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(flex: 2, child: Text('classGroup')),
+                      CustomButton(
+                        text: '戻る',
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
                     ],
                   ),
                 ],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  CustomButton(text: '戻る', onPressed: () {}),
-                  SizedBox(width: 16),
-                  CustomButton(text: '確認', onPressed: () {}),
-                  SizedBox(width: 16)
-                ],
-              )
-            ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildDetailRow(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
+          Text(
+            value,
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
       ),
     );
   }
