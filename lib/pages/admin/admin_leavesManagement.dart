ffc406e8078fba46c0b8ea4b8decf6a51effb387
AdiyaDede <gantulga.adiyadelger@gmail.com>
2024-12-11 11:25:15 +0900

    leaves page

--- lib/pages/admin/admin_leavesManagement.dart
+++ lib/pages/admin/admin_leavesManagement.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:sams/pages/testPages/testPages_leaves_edit.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'package:sams/widget/appbar.dart';
+import 'package:sams/widget/bottombar.dart';
+import 'package:sams/widget/button/custom_button.dart';
+
 class adminLeaveManagement extends StatefulWidget {
   const adminLeaveManagement({super.key});
 
@@ -89,103 +93,190 @@ class _adminLeaveManagementState extends State<adminLeaveManagement> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('休暇管理'),
-      ),
-      body: Column(
-        children: [
-          // フィルターと再読み込みボタン
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'フィルター: ',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      appBar: CustomAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Title Section
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  '休暇管理',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
-                DropdownButton<int>(
-                  value: filterStatus,
-                  items: const [
-                    DropdownMenuItem(
-                      value: 0,
-                      child: Text('未承認'),
+              ),
+            ),
+
+            // Filter and Reload Section
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      'フィルター: ',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
-                    DropdownMenuItem(
-                      value: 1,
-                      child: Text('承認済み'),
-                    ),
-                    DropdownMenuItem(
-                      value: 2,
-                      child: Text('却下'),
+                    DropdownButton<int>(
+                      value: filterStatus,
+                      items: const [
+                        DropdownMenuItem(value: 0, child: Text('未承認')),
+                        DropdownMenuItem(value: 1, child: Text('承認済み')),
+                        DropdownMenuItem(value: 2, child: Text('却下')),
+                      ],
+                      onChanged: (value) {
+                        if (value != null) {
+                          setState(() {
+                            filterStatus = value;
+                            applyFilter();
+                          });
+                        }
+                      },
                     ),
                   ],
-                  onChanged: (value) {
-                    if (value != null) {
-                      setState(() {
-                        filterStatus = value;
-                        applyFilter(); // フィルター結果を更新
-                      });
-                    }
-                  },
                 ),
-                const SizedBox(width: 16), // 空白スペース
                 IconButton(
                   icon: const Icon(Icons.refresh),
+                  onPressed: fetchAllLeaves,
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 16),
+
+            // Main Content Section
+
+            Expanded(
+              child: isManager == null
+                  ? const Center(child: CircularProgressIndicator())
+                  : isManager == false
+                      ? const Center(
+                          child: Text(
+                            '休暇管理の権限がありません',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        )
+                      : filteredLeaveData.isEmpty
+                          ? const Center(child: Text('休暇データがありません'))
+                          : ListView.builder(
+                              itemCount: filteredLeaveData.length,
+                              itemBuilder: (context, index) {
+                                final leave = filteredLeaveData[index];
+                                return Card(
+                                  elevation: 4,
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 8, vertical: 8),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  child: Stack(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(16),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              '授業名: ${leave["CLASS_NAME"] ?? "不明"}',
+                                              style: const TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            const SizedBox(height: 8),
+                                            Text(
+                                              '日付: ${leave["LEAVE_DATE"] ?? "不明"}\n'
+                                              '申請者: ${leave["USER_CLASS"] ?? "不明"}\t${leave["USER_NAME"] ?? "不明"}\n'
+                                              '種別: ${leave["LEAVE_CATEGORY"] ?? "不明"}\n'
+                                              '状態: ${{
+                                                    0: "未承認",
+                                                    1: "承認済み",
+                                                    2: "却下"
+                                                  }[leave["LEAVE_STATUS"]] ?? "不明"}',
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      Positioned(
+                                        top: 60,
+                                        right: 20,
+                                        child: InkWell(
+                                          onTap: () async {
+                                            final result = await Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder: (context) =>
+                                                    LeaveEditPage(
+                                                  leaveDetails: leave,
+                                                ),
+                                              ),
+                                            );
+                                            if (result == 'refresh') {
+                                              fetchAllLeaves();
+                                            }
+                                          },
+                                          child: Container(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 16, vertical: 8),
+                                            decoration: BoxDecoration(
+                                              color: Colors.blue,
+                                              borderRadius:
+                                                  BorderRadius.circular(8),
+                                            ),
+                                            child: const Text(
+                                              '休暇編集',
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            color: Colors.white,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CustomButton(
+                  text: '戻る',
                   onPressed: () {
-                    fetchAllLeaves(); // データを再取得
+                    Navigator.pop(context);
                   },
                 ),
               ],
             ),
           ),
-          // メインコンテンツ
-          Expanded(
-            child: isManager == null
-                ? const Center(child: CircularProgressIndicator()) // 確認中
-                : isManager == false
-                    ? const Center(
-                        child: Text(
-                          '休暇管理の権限がなかった',
-                          style: TextStyle(color: Colors.red),
-                        ),
-                      )
-                    : filteredLeaveData.isEmpty
-                        ? const Center(child: Text('休暇データがありません'))
-                        : ListView.builder(
-                            itemCount: filteredLeaveData.length,
-                            itemBuilder: (context, index) {
-                              final leave = filteredLeaveData[index];
-                              return Card(
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 16, vertical: 8),
-                                child: ListTile(
-                                  title: Text(
-                                    '授業名: ${leave["CLASS_NAME"] ?? "不明"}',
-                                    style: const TextStyle(fontSize: 16),
-                                  ),
-                                  subtitle: Text(
-                                    '日付: ${leave["LEAVE_DATE"] ?? "不明"}\n申請者: ${leave["USER_CLASS"] ?? "不明"}\t${leave["USER_NAME"] ?? "不明"}\n種別: ${leave["LEAVE_CATEGORY"] ?? "不明"}\n状態: ${leave["LEAVE_STATUS"] == 0 ? "未承認" : (leave["LEAVE_STATUS"] == 1 ? "承認済み" : "却下")}',
-                                  ),
-                                  onTap: () async {
-                                    final result = await Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => LeaveEditPage(
-                                          leaveDetails: leave,
-                                        ),
-                                      ),
-                                    );
-                                    // 前のページから戻ってきた時にデータを更新
-                                    if (result == 'refresh') {
-                                      fetchAllLeaves(); // データを再取得
-                                    }
-                                  },
-                                ),
-                              );
-                            },
-                          ),
-          ),
+          BottomBar(),
         ],
       ),
     );
