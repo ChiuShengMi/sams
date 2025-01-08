31ffb342603c765c78e4207236af5e48790573ac
East <adyureew956@gmail.com>
2025-01-08 10:33:09 +0900

    logdart

--- lib/utils/log.dart
+++ lib/utils/log.dart
@@ -4,31 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 class Utils {
   /// Firestore にログを保存する共通関数
   static Future<void> logMessage(String msg) async {
-    final user = FirebaseAuth.instance.currentUser;
-    if (user == null) throw Exception('ユーザーがログインしていません');
-    final uid = user.uid;
-
-    DocumentSnapshot? managerSnapshot;
-    managerSnapshot = await FirebaseFirestore.instance
-        .collection('Users')
-        .doc('Managers')
-        .collection('IT')
-        .doc(uid)
-        .get();
-
-    if (!managerSnapshot.exists) {
-      managerSnapshot = await FirebaseFirestore.instance
-          .collection('Users')
-          .doc('Managers')
-          .collection('GAME')
-          .doc(uid)
-          .get();
-    }
-
-    final managerData = managerSnapshot.data() as Map<String, dynamic>;
-    final userId = managerData['ID'];
-    final userName = managerData['NAME'];
-
     DateTime now = DateTime.now();
     String datePath =
         '${now.year}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')}';
@@ -50,6 +25,42 @@ class Utils {
       // エラーハンドリング（必要に応じてログ出力などを追加）
       print("Error saving log: $e");
     }
+  }
+
+  static Future<Map<String, String>> getUserInfo() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) throw Exception('ユーザーがログインしていません');
+    final uid = user.uid;
+
+    DocumentSnapshot? managerSnapshot;
+    managerSnapshot = await FirebaseFirestore.instance
+        .collection('Users')
+        .doc('Managers')
+        .collection('IT')
+        .doc(uid)
+        .get();
+
+    if (!managerSnapshot.exists) {
+      managerSnapshot = await FirebaseFirestore.instance
+          .collection('Users')
+          .doc('Managers')
+          .collection('GAME')
+          .doc(uid)
+          .get();
+    }
+
+    if (!managerSnapshot.exists) {
+      throw Exception('ユーザー情報が見つかりません');
+    }
+
+    final managerData = managerSnapshot.data() as Map<String, dynamic>;
+    final userId = managerData['ID'];
+    final userName = managerData['NAME'];
+
+    return {
+      'userId': userId,
+      'userName': userName,
+    };
   }
 }
 
